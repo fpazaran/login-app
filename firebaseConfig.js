76d0d762e0cac84a56820d9c95aba5386a577c35
fpazaran <fpazarann@gmail.com>
2024-01-30 20:30:26 -0800

    database code fix

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,13 +7,13 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 const firebaseConfig = {
   apiKey: "AIzaSyDqHIdp8xoe5ZRVYsa39pftNRfmkM85-gk",
   authDomain: "loginapp-2d801.firebaseapp.com",
-  databaseURL: "https://loginapp-2d801-default-rtdb.firebaseio.com",
+  databaseURL: "https://loginapp-2d801-default-rtdb.firebaseio.com/",
   projectId: "loginapp-2d801",
   storageBucket: "loginapp-2d801.appspot.com",
   messagingSenderId: "1046004927760",
   appId: "1:1046004927760:web:69763bfd1e5be43f37a995",
   measurementId: "G-WDZ8VV4H50",
-  databaseURL: "https://loginapp-2d801-default-rtdb.firebaseio.com"
+  databaseURL: "https://loginapp-2d801-default-rtdb.firebaseio.com/"
 };
 
 
--- signUp.js
+++ signUp.js
@@ -1,6 +1,6 @@
 import { Alert } from 'react-native';
 import { createUserWithEmailAndPassword } from '@firebase/auth';
-import { auth,db } from './firebaseConfig';
+import { auth, db } from './firebaseConfig';
 import { ref, set } from '@firebase/database';
 
 async function createProfile(user, name, email) {
@@ -15,7 +15,7 @@ export default async function signUp(email, password, name, {navigation}){
     if(email != '' && password != ''){
             await createUserWithEmailAndPassword(auth, email, password).then((userCredential) => {
                 console.log('auth passed')
-                createProfile(userCredential, name, email)
+                createProfile(userCredential.user, name, email)
                 Alert.alert('User Created')
                 navigation.navigate('WelcomeScreen')
                 
